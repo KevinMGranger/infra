587190713a9e1ad3209117aba03c911769657b48
Kevin M Granger <git@kevinmgranger.me>
2022-10-12 01:07:52 -0400

    add fish dotenv to shell prompt

--- config-files/config/fish/conf.d/fish_prompt.fish
+++ config-files/config/fish/conf.d/fish_prompt.fish
@@ -1,5 +1,3 @@
-# requires current-kube-context program
-
 function fish_prompt --description 'Write out the prompt'
     set stat $status
 
@@ -37,16 +35,32 @@ function fish_prompt --description 'Write out the prompt'
                 set -g __fish_prompt_cwd (set_color $fish_color_cwd)
             end
 
-            if which current-kube-context > /dev/null 2>/dev/null
+            if which current-kube-context > /dev/null 2>&1
                 set kube_ctx (current-kube-context 2>/dev/null); or set kube_ctx "unknown context"
             else
                 set kube_ctx "current-kube-context not installed"
             end
 
+            if which fishdotenv > /dev/null 2>&1
+                fishdotenv -c > /dev/null 2>&1
+                switch $status
+                    case 0 # in sync
+                        set dotenv_status "✔️"
+                    case 3 # file missing
+                        set dotenv_status "?"
+                    case 4 # out of sync
+                        set dotenv_status (echo (set_color --bold red) "OUT OF SYNC" (set_color normal))
+                    case '*' # some other error
+                        set dotenv_status (echo (set_color red) "e$status" (set_color normal))
+                end
+            else
+                set dotenv_status "fishdotenv not installed"
+            end
+
             set git (fish_git_prompt)
             if test $status -ne 0; set git "no git repo"; end
 
-            printf '[%s] %s%s@%s %s%s %s(%s)%s ⎇ %s ☸️ %s\f\r> ' (date "+%H:%M:%S") "$__fish_color_blue" $USER (prompt_hostname) "$__fish_prompt_cwd" "$PWD" "$__fish_color_status" "$stat" "$__fish_prompt_normal" $git $kube_ctx
+            printf '[%s] %s%s@%s %s%s %s(%s)%s ⎇ %s ☸️ %s 🇪 %s\f\r> ' (date "+%H:%M:%S") "$__fish_color_blue" $USER (prompt_hostname) "$__fish_prompt_cwd" "$PWD" "$__fish_color_status" "$stat" "$__fish_prompt_normal" $git $kube_ctx $dotenv_status
 
     end
 end
