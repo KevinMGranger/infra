50f032017a773a0bf0e05061fe9e1a95a8137dfb
Kevin M Granger <git@kevinmgranger.me>
2022-10-12 01:08:15 -0400

    clean up mac aliases, make python work

--- config-files/mac.fish
+++ config-files/mac.fish
@@ -1,10 +1,11 @@
 eval (/opt/homebrew/bin/brew shellenv)
 
-# todo: is this still necessary?
-abbr -a ansible-playbook env OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES ANSIBLE_NOCOWS=1 ansible-playbook
-
 alias bup "brew upgrade"
 
-fish_add_path ~/Library/Python/3.10/bin
+for python in /opt/homebrew/opt/python@3.*
+    fish_add_path $python/bin
+end
+
+fish_add_path ~/Library/Python/3.10/bin # pip install --user
 
 alias tailscale /Applications/Tailscale.app/Contents/MacOS/Tailscale
