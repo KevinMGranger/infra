f7232e40a7fbde4f13782a30dfa5803f323e881d
Kevin M Granger <git@kevinmgranger.me>
2022-11-09 13:31:52 -0500

    fix c func

--- config-files/config/fish/functions/c.fish
+++ config-files/config/fish/functions/c.fish
@@ -1,8 +1,8 @@
 function c -d "Display directory or file contents" -a target
     if test -z $target
         or test -d $target
-        exa $target $argv
+        exa $target $argv[2..]
     else
-        bat $target $argv
+        bat $target $argv[2..]
     end
 end
