24df6f6f822dcee22e6eb383b3d0f15293b3963b
Kevin M Granger <git@kevinmgranger.me>
2024-06-14 11:13:41 -0400

    finish overhauling most roles to support their new homes, and transition away from exa to eza

--- ansible-infra-collection/roles/daily_driver/files/config/fish/functions/c.fish
+++ ansible-infra-collection/roles/daily_driver/files/config/fish/functions/c.fish
@@ -1,7 +1,7 @@
 function c -d "Display directory or file contents" -a target
     if test -z $target
         or test -d $target
-        exa $target $argv[2..]
+        eza $target $argv[2..]
     else
         bat $target $argv[2..]
     end
