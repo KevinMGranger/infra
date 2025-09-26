c4b1e0189292aaec8aafba5cbcf90097988fd4e2
Kevin M Granger <git@kevinmgranger.me>
2025-09-26 19:05:47 -0400

    uppies

--- dev-container-bootstrap.sh
+++ dev-container-bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo dnf install ansible -y
 
-ansible-galaxy collection install ./ansible-infra-collection
+ansible-galaxy collection install --force ./ansible-infra-collection
 
 ansible-playbook kmg.infra.daily_driver -i ./inventories/local.ini
