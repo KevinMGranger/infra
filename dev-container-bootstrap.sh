df8806cb55c5371b64a157cfe03833acdf9de974
Kevin M Granger <git@kevinmgranger.me>
2025-11-21 20:57:13 -0500

    slootstrap

--- dev-container-bootstrap.sh
+++ dev-container-bootstrap.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 sudo dnf install ansible -y
 
+# TODO: need to cd here
+
 ansible-galaxy collection install --force ./ansible-infra-collection
 
 ansible-playbook kmg.infra.daily_driver -i ./inventories/local.ini -e vscode_fish=true
+
+# TODO: probably make this part of ansible?
+
+mkdir ~/.bashrc.d
+echo "if which fish; exec fish; else echo 'no fish'; fi" >> ~/.bashrc.d/fishy
