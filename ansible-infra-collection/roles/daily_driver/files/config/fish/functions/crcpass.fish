f9a54b51a0c628f6bdd5f71ed74a65fd12ffa6f7
Kevin M Granger <git@kevinmgranger.me>
2026-04-16 11:18:57 -0400

    make crcpass work locally

--- ansible-infra-collection/roles/daily_driver/files/config/fish/functions/crcpass.fish
+++ ansible-infra-collection/roles/daily_driver/files/config/fish/functions/crcpass.fish
@@ -1,3 +1,7 @@
-function crcpass -d "get the kubeadmin password from CRC running on the given machine"
-    ssh $argv 'cat .crc/machines/crc/kubeadmin-password'
+function crcpass -d "get the kubeadmin password from CRC running on the given machine, or the current machine if unspecified"
+    if count $argv
+        ssh $argv 'cat .crc/machines/crc/kubeadmin-password'
+    else
+        cat .crc/machines/crc/kubeadmin-password
+    end
 end
